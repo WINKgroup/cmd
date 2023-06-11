dd1a1228d054f7aff50588244585a3d240cddb0c
Domenico Pontari <fairsayan@gmail.com>
2023-06-11 15:19:26 +0200

    options simplified

--- src/index.ts
+++ src/index.ts
@@ -1,143 +1,150 @@
-import ConsoleLog, { ConsoleLogGeneralOptions, ConsoleLogLevelOptions, LogLevel } from '@winkgroup/console-log'
-import { ChildProcessWithoutNullStreams, spawn } from "child_process"
-import _ from "lodash"
-import type { PartialDeep } from 'type-fest'
-import { CmdStreamManager, CmdStreamManagerOptions } from './streamManager'
+import ConsoleLog, { LogLevel } from '@winkgroup/console-log';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import _ from 'lodash';
+import { CmdStreamManager, CmdStreamManagerOptions } from './streamManager';
 
 export interface CmdOptions {
-    getResult: boolean // shortcut option to setup stdout & stderr options
-    timeout: number
-    args: string[]
-    spawnOptions?:any
-    consoleLogGeneralOptions: ConsoleLogGeneralOptions
-    consoleLogLevelOptions?: {[key: string]: PartialDeep<ConsoleLogLevelOptions>}
-    stdoutOptions?:Partial<CmdStreamManagerOptions>
-    stderrOptions?:Partial<CmdStreamManagerOptions>
+    getResult: boolean; // shortcut option to setup stdout & stderr options
+    timeout: number;
+    args: string[];
+    spawnOptions?: any;
+    consoleLog?: ConsoleLog;
+    stdoutOptions?: Partial<CmdStreamManagerOptions>;
+    stderrOptions?: Partial<CmdStreamManagerOptions>;
 }
 
 export default class Cmd {
-    timeout: number
-    cmd:string
-    args = [] as string[]
-    stdout:CmdStreamManager
-    stderr:CmdStreamManager
-    exitCode = null as number | null
-    spawnOptions?:any
-    consoleLog:ConsoleLog
-    private childProcess = null as ChildProcessWithoutNullStreams | null
+    timeout: number;
+    cmd: string;
+    args = [] as string[];
+    stdout: CmdStreamManager;
+    stderr: CmdStreamManager;
+    exitCode = null as number | null;
+    spawnOptions?: any;
+    consoleLog: ConsoleLog;
+    private childProcess = null as ChildProcessWithoutNullStreams | null;
 
-    constructor(cmd:string, inputOptions?:Partial<CmdOptions>) {
-        const options:CmdOptions = _.defaults(inputOptions, {
+    constructor(cmd: string, inputOptions?: Partial<CmdOptions>) {
+        const options: CmdOptions = _.defaults(inputOptions, {
             getResult: true,
             timeout: 20,
             args: [] as string[],
-            consoleLogGeneralOptions: { prefix: 'Cmd' }
-        })
+        });
 
-        this.cmd = cmd
-        this.timeout = options.timeout
-        this.args = options.args
+        this.cmd = cmd;
+        this.timeout = options.timeout;
+        this.args = options.args;
 
-        this.stdout = new CmdStreamManager('stdout', options.consoleLogGeneralOptions, options.stdoutOptions)
-        this.stderr = new CmdStreamManager('stderr', options.consoleLogGeneralOptions, options.stderrOptions)
-        this.spawnOptions = options.spawnOptions
+        this.stdout = new CmdStreamManager('stdout', options.stdoutOptions);
+        this.stderr = new CmdStreamManager('stderr', options.stderrOptions);
+        this.spawnOptions = options.spawnOptions;
 
-        this.consoleLog = new ConsoleLog(options.consoleLogGeneralOptions)
+        this.consoleLog = options.consoleLog
+            ? options.consoleLog
+            : new ConsoleLog({ prefix: 'Cmd' });
 
-        this.stdout.collectDataAsString = options.getResult
-        this.stderr.collectDataAsString = options.getResult
+        this.stdout.collectDataAsString = options.getResult;
+        this.stderr.collectDataAsString = options.getResult;
     }
 
     start() {
-        const commandStr = this.cmdStr()
+        const commandStr = this.cmdStr();
         if (this.childProcess) {
-            this.consoleLog.warn(`${commandStr} already started: not starting again`)
-            return
+            this.consoleLog.warn(
+                `${commandStr} already started: not starting again`
+            );
+            return;
         }
-        this.consoleLog.print(commandStr)
-        this.stdout.data = ''
-        this.stderr.data = ''
-        this.exitCode = null
+        this.consoleLog.print(commandStr);
+        this.stdout.data = '';
+        this.stderr.data = '';
+        this.exitCode = null;
 
         try {
-            this.childProcess = spawn(this.cmd, this.args, this.spawnOptions)
+            this.childProcess = spawn(this.cmd, this.args, this.spawnOptions);
         } catch (e) {
-            this.stderr.getNewData(e as Error | string)
+            this.stderr.getNewData(e as Error | string, this.consoleLog);
         }
-        let timeoutObj = null as NodeJS.Timeout | null
+        let timeoutObj = null as NodeJS.Timeout | null;
         if (!this.childProcess) {
-            this.childProcess = null
-            return this.childProcess
+            this.childProcess = null;
+            return this.childProcess;
         }
 
-        this.childProcess.on('error', (error) => { this.stderr.getNewData( error ) })
+        this.childProcess.on('error', (error) => {
+            this.stderr.getNewData(error, this.consoleLog);
+        });
 
-        if (this.childProcess.stderr) this.childProcess.stderr.on('data', (data) => {
-            this.stderr.getNewData( data )
-        })
+        if (this.childProcess.stderr)
+            this.childProcess.stderr.on('data', (data) => {
+                this.stderr.getNewData(data, this.consoleLog);
+            });
 
-        if (this.childProcess.stdout) this.childProcess.stdout.on('data', (data) => {
-            this.stdout.getNewData( data )
-        })
+        if (this.childProcess.stdout)
+            this.childProcess.stdout.on('data', (data) => {
+                this.stdout.getNewData(data, this.consoleLog);
+            });
 
         this.childProcess.on('close', (code) => {
             if (timeoutObj) {
-                clearTimeout(timeoutObj)
-                timeoutObj = null
+                clearTimeout(timeoutObj);
+                timeoutObj = null;
             }
-            this.childProcess = null
-            this.exitCode = code
-            if ( code !== 0 ) this.consoleLog.error(`exit with code ${this.exitCode}`)
-        })
+            this.childProcess = null;
+            this.exitCode = code;
+            if (code !== 0)
+                this.consoleLog.error(`exit with code ${this.exitCode}`);
+        });
 
         if (this.timeout) {
-            timeoutObj = setTimeout( () => {
-                this.stderr.getNewData( 'TIMEOUT' )
-                this.kill()
-            }, this.timeout * 1000)
+            timeoutObj = setTimeout(() => {
+                this.stderr.getNewData('TIMEOUT', this.consoleLog);
+                this.kill();
+            }, this.timeout * 1000);
         }
 
-        return this.childProcess
+        return this.childProcess;
     }
 
     kill() {
         if (this.childProcess) {
-            this.childProcess.stdin.end()
-            this.childProcess.stdout.destroy()
-            this.childProcess.stderr.destroy()
-            this.childProcess.kill()
-        }
-            else console.error('unable to kill Cmd: no child process')
+            this.childProcess.stdin.end();
+            this.childProcess.stdout.destroy();
+            this.childProcess.stderr.destroy();
+            this.childProcess.kill();
+        } else console.error('unable to kill Cmd: no child process');
     }
 
     run() {
-        return new Promise<Cmd>( (resolve) => {
-            this.start()
-            
+        return new Promise<Cmd>((resolve) => {
+            this.start();
+
             if (this.childProcess)
-                this.childProcess.on('close', () => { resolve(this) })
-                else resolve(this)
-        })
+                this.childProcess.on('close', () => {
+                    resolve(this);
+                });
+            else resolve(this);
+        });
     }
 
     cmdStr() {
-        let trailer = this.args.length > 0 ? this.args.join(' ') : ''
-        if (trailer) trailer = ' ' + trailer
-        return this.cmd + trailer
+        let trailer = this.args.length > 0 ? this.args.join(' ') : '';
+        if (trailer) trailer = ' ' + trailer;
+        return this.cmd + trailer;
     }
 
-    static run(cmd:string, inputOptions?:Partial<CmdOptions>) {
-        const command = new Cmd(cmd, inputOptions)
-        return command.run()
+    static run(cmd: string, inputOptions?: Partial<CmdOptions>) {
+        const command = new Cmd(cmd, inputOptions);
+        return command.run();
     }
 
-    static async exists(cmd:string) {
-        const options:Partial<CmdOptions> = {
-            consoleLogGeneralOptions: { verbosity: LogLevel.NONE },
-            args: ['-v', cmd]
-        }
+    static async exists(cmd: string) {
+        const options: Partial<CmdOptions> = {
+            consoleLog: new ConsoleLog({ verbosity: LogLevel.NONE }),
+            args: ['-v', cmd],
+        };
 
-        const command = await this.run('command', options)
-        return (command.exitCode === 0)
+        const command = await this.run('command', options);
+        return command.exitCode === 0;
     }
 }
--- src/streamManager.ts
+++ src/streamManager.ts
@@ -1,40 +1,41 @@
-import ConsoleLog, { ConsoleLogLevelOptions, ConsoleLogGeneralOptions, LogLevel } from '@winkgroup/console-log'
-import _ from 'lodash'
-import { EventEmitter } from 'node:events'
-import { PartialDeep } from 'type-fest'
+import ConsoleLog, { LogLevel } from '@winkgroup/console-log';
+import _ from 'lodash';
+import { EventEmitter } from 'node:events';
 
 export interface CmdStreamManagerOptions {
-    collectDataAsString: boolean
-    logLevel: LogLevel
-    consoleLogLevelOptions?: {[key: string]: PartialDeep<ConsoleLogLevelOptions>}
+    collectDataAsString: boolean;
+    logLevel: LogLevel;
 }
 
-export class CmdStreamManager extends EventEmitter  {
-    name: 'stdout' | 'stderr'
-    collectDataAsString: boolean
-    logLevel: LogLevel
-    consoleLog: ConsoleLog
-    data = ''
+export class CmdStreamManager extends EventEmitter {
+    name: 'stdout' | 'stderr';
+    collectDataAsString: boolean;
+    logLevel: LogLevel;
+    data = '';
 
-    constructor(name: 'stdout' | 'stderr', consoleLogGeneralOptions: ConsoleLogGeneralOptions, inputOptions?:Partial<CmdStreamManagerOptions>) {
-        super()
-        this.name = name
+    constructor(
+        name: 'stdout' | 'stderr',
+        inputOptions?: Partial<CmdStreamManagerOptions>
+    ) {
+        super();
+        this.name = name;
         const options = _.defaults(inputOptions, {
             collectDataAsString: true,
             logLevel: name === 'stdout' ? LogLevel.INFO : LogLevel.ERROR,
-        })
+        });
 
-        this.collectDataAsString = options.collectDataAsString
-        this.logLevel = options.logLevel
-        this.consoleLog = new ConsoleLog(consoleLogGeneralOptions, options.consoleLogLevelOptions)
-        if (!this.consoleLog.generalOptions.id) this.consoleLog.generalOptions.id = name
+        this.collectDataAsString = options.collectDataAsString;
+        this.logLevel = options.logLevel;
     }
 
-    getNewData(inputNewData:Error | string) {
-        const newData = typeof inputNewData === 'string' ? inputNewData : inputNewData.toString()
-        if (this.collectDataAsString) this.data += newData
-        this.emit('data', inputNewData)
-        this.consoleLog.print(newData, this.logLevel)
-        return newData
+    getNewData(inputNewData: Error | string, consoleLog: ConsoleLog) {
+        const newData =
+            typeof inputNewData === 'string'
+                ? inputNewData
+                : inputNewData.toString();
+        if (this.collectDataAsString) this.data += newData;
+        this.emit('data', inputNewData);
+        consoleLog.print(newData, this.logLevel);
+        return newData;
     }
 }
